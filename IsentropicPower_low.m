50715fdd699e1f04d7121b1e65386bebce5f8503
Nathan Green <njgreen3@alaska.edu>
2018-07-13 16:54:25 -0800

    Cleaned up Turbine/pump block scripts

--- IsentropicPower_low.m
+++ IsentropicPower_low.m
@@ -13,7 +13,7 @@ function [ P_hy, T_or_H_o ] = IsentropicPower_low( T_or_H_i, p_i, p_o, m_dot, fl
 %   p_i         Input pressure of the fluid in Pa
 %   p_o         Output temperature of the fluid in Pa
 %   m_dot       Mass flow rate of the fluid in kg/s 
-%   fluid       Name of the working fluid
+%   fluid       Initialzed coolprop fluid handle
 %   isen_eff    Isentropic efficiency of the device. Expected range: 0-1
 %               This input is optional. If it is not provided then it will 
 %               be set to 1, which is the efficiency of an ideal isentropic
@@ -26,15 +26,15 @@ function [ P_hy, T_or_H_o ] = IsentropicPower_low( T_or_H_i, p_i, p_o, m_dot, fl
 %               enthalpy increases (energy in added to the system) then the
 %               efficiency is divided by the change in energy because more
 %               energy is required than the ideal case.
-%   T_or_H      String stating whether arguements 1&5 are temperature (T) 
-%               or enthaply (H). Also applies to output
+%   T_or_H      Logical indicating whether the relavent input and output
+%               variables are temperature (1) or enthalpy (0)
 % 
 % Outputs:
 %   P_hy        Hydraulic power exctracted or consumed in W. Positive
 %               values indicate the system is providing hydraulic power as
 %               a turbine or expander. Negative values indicate the system 
 %               is consuming hydraulic power as a compressor or pump.
-%   T_o         Output temperature in Kelvin or enthalpy in J/kg
+%   T_or_H_o    Output temperature in Kelvin or enthalpy in J/kg
 
 % This function makes use of the CoolProp wrapper for fluid properties. 
 % The high level Matlab wrapper cannot handle vector inputs. There ways to
@@ -52,18 +52,12 @@ buffer_size = 255;
 ierr = 0;
 b = (1:1:buffer_size);
 herr= char(b);
-
 coder.extrinsic('calllib') 
 coder.extrinsic('libpointer') 
 
 % All the input signals should be the same size, so it shouldn't matter
 % which one is used to obtain signal length
 var_length = length(T_or_H_i);
-% T_or_H_i
-
-fluid = 'R245fa';
-% initialize handles for hot and cool fluids
-[handle, ~] = calllib('coolprop','AbstractState_factory',backend,fluid,ierr,herr,buffer_size);
 
 % determine input parameter pair indecies for coolprop calls
 % [pq_ind,~] = calllib('coolprop','get_input_pair_index','PQ_INPUTS'); %pressure and Mass vapor quality
@@ -81,22 +75,8 @@ fluid = 'R245fa';
 p_i_Ptr = libpointer('doublePtr',p_i);
 p_o_Ptr = libpointer('doublePtr',p_o);
 
-% % Check inputs
-% if nargin < 7
-% %   Set equal to 1 if isen_eff is not in input
-%     isen_eff = 1;
-%     
-%     if nargin < 6
-%         error(message('NotEnoughInputs'));
-%     end
-% end
-% % check if isen_eff is in range
-% if isen_eff > 1 || isen_eff < 0
-%     error(message('isen_eff is out of range'));
-% end
-
 % Ensure temperature/enthalpy input variables are delt with appropriately
-if upper(T_or_H) == 'T' %|| 'TEMPERATURE'
+if T_or_H 
 % Assume given temperature values are not at vaporization/condensation.
 % Initialize T and H inputs
     T_i = T_or_H_i;
@@ -104,35 +84,34 @@ if upper(T_or_H) == 'T' %|| 'TEMPERATURE'
     H_i = zeros(var_length,1); % initialized to 0 temporarily
     H_i_Ptr = libpointer('doublePtr',H_i);
 % Update values located at Hhi and Hci pointer locations
-    calllib('coolprop','AbstractState_update_and_1_out', handle, pt_ind, p_i_Ptr, T_i_Ptr, var_length, h_ind, H_i_Ptr, ierr,herr,buffer_size);
+    calllib('coolprop','AbstractState_update_and_1_out', fluid, pt_ind, p_i_Ptr, T_i_Ptr, var_length, h_ind, H_i_Ptr, ierr,herr,buffer_size);
 % Set Hhi & Hci to values stored at pointer location
     H_i = get(H_i_Ptr,'Value');
     
-elseif upper(T_or_H) == 'H' %|| 'ENTHALPY'
+else
 % Enthalpy values can be in vaporization/condensation region  
 % Initialize H values
     H_i = T_or_H_i;
     H_i_Ptr = libpointer('doublePtr',H_i);
-else
-    error('Please specify whether input uses variables of Temperature or Enthalpy.')
+
 end
 
 % Input entropy in J/kg/K
 % Initialize pointer locations for relevant variables
 S_i_Ptr = libpointer('doublePtr', zeros(var_length,1)); 
 % Update values at S_i pointer location
-calllib('coolprop','AbstractState_update_and_1_out', handle, hp_ind, H_i_Ptr, p_i_Ptr, var_length, s_ind, S_i_Ptr, ierr,herr,buffer_size);
+calllib('coolprop','AbstractState_update_and_1_out', fluid, hp_ind, H_i_Ptr, p_i_Ptr, var_length, s_ind, S_i_Ptr, ierr,herr,buffer_size);
 % Set S_i to values stored at pointer location
 % S_i = get(S_i_Ptr,'Value');
 
 % Ideal Output entropy is equal to input entropy 
 % Ideal Output enthalpy in J/kg and temperature
-H_o_ideal_Ptr = libpointer('doublePtr',zeros(var_length,1));
+H_o_ideal = zeros(var_length,1);
+H_o_ideal_Ptr = libpointer('doublePtr',H_o_ideal);
 % Update values located at Hhi and Hci pointer locations
-calllib('coolprop','AbstractState_update_and_1_out', handle, ps_ind, p_o_Ptr, S_i_Ptr, var_length, h_ind, H_o_ideal_Ptr, ierr,herr,buffer_size);
+calllib('coolprop','AbstractState_update_and_1_out', fluid, ps_ind, p_o_Ptr, S_i_Ptr, var_length, h_ind, H_o_ideal_Ptr, ierr,herr,buffer_size);
 % Set H_o_ideal to values stored at pointer location
 H_o_ideal = get(H_o_ideal_Ptr,'Value');
-% T_o_ideal = CoolProp.PropsSI('T', 'P', p_o, 'S', S_i, fluid);
 
 % Calculate Ideal power consumed/provided
 P_ideal = m_dot.*(H_i - H_o_ideal);
@@ -149,30 +128,21 @@ P_hy(P_ideal > 0) = isen_eff.*P_ideal(P_ideal > 0);
 %   This means the actual power consumed is greater than the ideal power.
 P_hy(P_ideal < 0) = P_ideal(P_ideal < 0)./isen_eff;
 
-% %   If the ideal power consumed/provided is 0 then either the fluid did not 
-% %   undergo any change or some (non-ideal) work was consumed to change it's
-% %   state
-%     warning('Actual mechanical power cannot be determined from the information provided.');
-%     P_hy = NaN;
-%     T_o = T_i;
-%     return;
-
-
+% Set output enthalpy
 H_o = H_i - P_hy./m_dot;
 H_o_Ptr = libpointer('doublePtr',H_o);
-% T_o = CoolProp.PropsSI('T', 'P', p_o, 'H', H_o, fluid);
 
 % Ensure temperature/enthalpy input variables are delt with appropriately
-if upper(T_or_H) == 'T' %|| 'TEMPERATURE'
+if T_or_H 
 % Assume given temperature values are not at vaporization/condensation
-    T_or_H_o_Ptr = libpointer('doublePtr',zeros(var_length,1));
-    calllib('coolprop','AbstractState_update_and_1_out', handle, hp_ind, H_o_Ptr, p_o_Ptr, var_length, t_ind, T_or_H_o_Ptr, ierr,herr,buffer_size);
+    T_or_H_o = zeros(var_length,1);
+    T_or_H_o_Ptr = libpointer('doublePtr',T_or_H_o);
+    calllib('coolprop','AbstractState_update_and_1_out', fluid, hp_ind, H_o_Ptr, p_o_Ptr, var_length, t_ind, T_or_H_o_Ptr, ierr,herr,buffer_size);
     T_or_H_o = get(T_or_H_o_Ptr,'Value');
     
-elseif upper(T_or_H) == 'H' %|| 'ENTHALPY'
+else
 % Enthalpy values can be in vaporization/condensation region    
     T_or_H_o = H_o;
 end
-calllib('coolprop','AbstractState_free', handle, ierr,herr,buffer_size);
 end
 
